2636c252803ba3bd8b384c2ff1c0cdbe071bdcd9
reskillfrom31-lab <re.skill.from31@gmail.com>
2025-09-28 23:25:11 +0900

    不要なコードをコメントアウト【add_details_to_prototypes.rb】

--- db/migrate/20250925130516_add_details_to_prototypes.rb
+++ db/migrate/20250925130516_add_details_to_prototypes.rb
@@ -1,6 +1,6 @@
 class AddDetailsToPrototypes < ActiveRecord::Migration[7.1]
   def change
-    add_column :prototypes, :title, :string, null: false
+    #add_column :prototypes, :title, :string, null: false
     add_column :prototypes, :catch_copy, :text, null: false
     add_column :prototypes, :concept, :text, null: false
     add_reference :prototypes, :user, null: false, foreign_key: true
