d4990fc5f620044e6fcd5792a486b465949c3361
reskillfrom31-lab <re.skill.from31@gmail.com>
2025-09-29 16:39:36 +0900

    メンター指摘事項修正
    ①ユーザーモデルにバリデーションを追加
    ②フォームビューのクラス連携ができていなかった部分を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :prototypes
   has_many :comments
+  validates :name, :profile, :occupation, :position, presence: true
 end
